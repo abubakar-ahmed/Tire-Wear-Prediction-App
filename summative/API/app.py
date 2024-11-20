ceed9434f69be6a7e19faf6fcf03beb1aab11d93
Abubakar Ahmed <128506001+abubakar-ahmed@users.noreply.github.com>
2024-11-20 09:49:57 +0100

    Update app.py

--- summative/API/app.py
+++ summative/API/app.py
@@ -1,6 +1,6 @@
 import os
 
-base_dir = os.path.dirname(os.path.abspath(_file_))
+base_dir = os.path.dirname(os.path.abspath(__file__))
 print("Base directory contents:", os.listdir(base_dir))
 print("Parent directory contents:", os.listdir(os.path.join(base_dir, "..")))
 print("Linear regression model directory contents:", os.listdir(os.path.join(base_dir, "../linear_regression/model")))
